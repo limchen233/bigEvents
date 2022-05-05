2b6aeffd032a79ecd14ce3e9bb580ca369188b7d
chenlim <chenlim233@163.com>
2022-05-05 19:50:44 +0800

    暴露路由并导入路由

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.use(cors())
 // 配置解析表单数据的中间件(此中间件只能解析'application/x-www-form-urlencoded'格式的表单)
 app.use(express.urlencoded({ extended: false }))
 
+// 导入并注册用户路由模块
+const userRouter = require('./router/user')
+app.use('/api', userRouter)
+
 // 启动服务器
 app.listen(3007, () => {
 	console.log('server running at http://127.0.0.1:3007')
