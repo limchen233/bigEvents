159d8d409f2b6f2b023aaad9a7461547da3dda56
chenlim <chenlim233@163.com>
2022-05-06 09:14:27 +0800

    抽离路由模块

--- router/user.js
+++ router/user.js
@@ -3,15 +3,13 @@ const express = require('express')
 // 创建路由对象
 const router = express.Router()
 
+// 导入用户路由处理函数模块
+const user_handler = require('../router_handler/user')
 // 注册新用户
-router.post('/register', (req, res) => {
-	res.send('register ok')
-})
+router.post('/register', user_handler.register)
 
 // 登录
-router.post('/login', (req, res) => {
-	res.send('login ok')
-})
+router.post('/login', user_handler.login)
 
 // 将路由对象共享出去
 module.exports = router
