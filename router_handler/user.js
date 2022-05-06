b32f9a67076a3d69a9e0d2d7a1ac3f71ab787ccf
chenlim <chenlim233@163.com>
2022-05-06 16:57:04 +0800

    封装统一验证响应的函数

--- router_handler/user.js
+++ router_handler/user.js
@@ -1,4 +1,3 @@
-const { send } = require('express/lib/response')
 const db = require('../db')
 
 const bcrypt = require('bcryptjs')
@@ -18,11 +17,14 @@ exports.register = (req, res) => {
 	db.query(sqlStr, userinfo.username, (err, result) => {
 		// 执行sql语句失败
 		if (err) {
-			return res.send({ status: 1, msg: err.message })
+			// return res.send({ status: 1, msg: err.message })
+
+			return res.cc(err)
 		}
 		// 判断用户名是否被占用
 		if (result.length > 0) {
-			return res.send({ status: 1, msg: '用户名被占用，请更换其它用户名！' })
+			// return res.send({ status: 1, msg: '用户名被占用，请更换其它用户名！' })
+			return res.cc('用户名被占用，请更换其它用户名！')
 		}
 
 		// 对密码进行加密
@@ -35,13 +37,16 @@ exports.register = (req, res) => {
 		// 执行sql语句
 		db.query(sql, { username: userinfo.username, password: userinfo.password }, (err, result) => {
 			// 判断sql是否执行成功
-			if (err) return res.send({ status: 1, msg: err.message })
+			// if (err) return res.send({ status: 1, msg: err.message })
+			if (err) return res.cc(err)
 
 			// 判断影响行数是否为1
-			if (result.affectedRows !== 1) return res.send({ status: 1, msg: '注册用户失败！' })
+			// if (result.affectedRows !== 1) return res.send({ status: 1, msg: '注册用户失败！' })
+			if (result.affectedRows !== 1) return res.cc('注册用户失败！')
 
 			// 注册用户成功
-			res.send({ status: 0, msg: '注册成功！' })
+			// res.send({ status: 0, msg: '注册成功！' })
+			res.cc('注册成功！', 0)
 		})
 	})
 }
