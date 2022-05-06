dfeb2ea02fd4874b06141c24d115bbfc9cee1c02
chenlim <chenlim233@163.com>
2022-05-06 16:04:00 +0800

    定义插入用户sql语句，判断是否插入成功

--- router_handler/user.js
+++ router_handler/user.js
@@ -13,7 +13,7 @@ exports.register = (req, res) => {
 		return res.send({ status: 1, msg: '用户名或密码不能为空！' })
 	}
 
-	// 定义sql语句，查询用户名是否被占用
+	// 定义sql语句，查询用户名
 	const sqlStr = 'select * from ev_users where username = ?'
 	db.query(sqlStr, userinfo.username, (err, result) => {
 		// 执行sql语句失败
@@ -24,14 +24,26 @@ exports.register = (req, res) => {
 		if (result.length > 0) {
 			return res.send({ status: 1, msg: '用户名被占用，请更换其它用户名！' })
 		}
+
 		// 对密码进行加密
 		userinfo.password = bcrypt.hashSync(userinfo.password, 10)
 		// console.log(userinfo)
 
-		return res.send('register successfully!')
-	})
+		// 定义插入用户的 SQL 语句
+		const sql = 'insert into ev_users set ?'
 
-	// res.send('register ok')
+		// 执行sql语句
+		db.query(sql, { username: userinfo.username, password: userinfo.password }, (err, result) => {
+			// 判断sql是否执行成功
+			if (err) return res.send({ status: 1, msg: err.message })
+
+			// 判断影响行数是否为1
+			if (result.affectedRows !== 1) return res.send({ status: 1, msg: '注册用户失败！' })
+
+			// 注册用户成功
+			res.send({ status: 0, msg: '注册成功！' })
+		})
+	})
 }
 
 // 登录的处理函数
