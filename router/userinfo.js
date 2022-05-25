e354704cb059422422c38ac019c1c56a139ade44
chenlim <chenlim233@163.com>
2022-05-25 15:54:04 +0800

    实现更新密码功能

--- router/userinfo.js
+++ router/userinfo.js
@@ -9,7 +9,7 @@ const userinfo_handler = require('../router_handler/userinfo')
 const expressJoi = require('@escook/express-joi')
 
 // 2、导入需要的验证规则对象
-const { update_userinfo_schema } = require('../schema/user')
+const { update_userinfo_schema, update_password_schema } = require('../schema/user')
 
 // 获取用户信息的基本路由
 router.get('/userinfo', userinfo_handler.getUserInfo)
@@ -18,6 +18,6 @@ router.get('/userinfo', userinfo_handler.getUserInfo)
 router.post('/userinfo', expressJoi(update_userinfo_schema), userinfo_handler.updateUserInfo)
 
 // 更新密码的路由
-router.post('/updatepwd', userinfo_handler.updatePassword)
+router.post('/updatepwd', expressJoi(update_password_schema), userinfo_handler.updatePassword)
 
 module.exports = router
--- router_handler/userinfo.js
+++ router_handler/userinfo.js
@@ -1,5 +1,7 @@
 // 导入数据库操作模块
 const db = require('../db/index')
+// 导入处理密码的模块
+const bcrypt = require('bcryptjs')
 
 // 获取用户基本信息的处理函数
 exports.getUserInfo = (req, res) => {
@@ -43,5 +45,38 @@ exports.updateUserInfo = (req, res) => {
 
 // 更新用户密码的处理函数
 exports.updatePassword = (req, res) => {
-	res.send('ok')
+	// 根据id查询用户信息
+	const sql = 'select * from ev_users where id = ?'
+
+	// 执行sql
+	db.query(sql, req.user.id, (err, result) => {
+		// 执行sql语句失败
+		if (err) return res.cc(err)
+
+		// 判断结果是否存在
+		if (result.length !== 1) return res.cc('用户不存在！')
+
+		// 判断输入的旧密码是否正确
+		const compareResult = bcrypt.compareSync(req.body.oldPwd, result[0].password)
+		if (!compareResult) return res.cc('旧密码错误！')
+
+		/* 更新数据库中的密码 */
+		// 定义更新密码的sql
+		const sql = 'update ev_users set password=? where id=?'
+
+		// 对新密码进行加密处理
+		const newPwd = bcrypt.hashSync(req.body.newPwd, 10)
+
+		// 执行sql
+		db.query(sql, [newPwd, req.user.id], (err, result) => {
+			// 执行sql语句失败
+			if (err) return res.cc(err)
+
+			// 判断影响行数是否为1
+			if (result.affectedRows !== 1) return res.cc('更新密码失败！')
+
+			// 成功
+			res.cc('更新密码成功！', 0)
+		})
+	})
 }
