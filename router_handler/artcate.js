415f8c9142418f00062f4d78c27d29ab7dd8fd14
chenlim <chenlim233@163.com>
2022-05-27 16:49:40 +0800

    添加查询文章列表接口

--- router_handler/artcate.js
+++ router_handler/artcate.js
@@ -1,6 +1,20 @@
 // 路由处理函数模块
+// 导入数据库操作模块
+const db = require('../db/index')
 
 // 获取文章分类列表的处理函数
 exports.getArtCate = (req, res) => {
-	res.send('ok')
+	// 定义查询文章分类列表的sql
+	const sql = 'select * from ev_article_cate where is_delete=0 order by id asc'
+
+	// 执行sql语句
+	db.query(sql, (err, result) => {
+		if (err) return res.cc(err)
+
+		res.send({
+			status: 0,
+			message: '获取文章分类数据成功!',
+			data: result
+		})
+	})
 }
