c4191e9a47e29cc8890b8ca8dedff152ab96b74e
chenlim <chenlim233@163.com>
2022-05-27 16:34:42 +0800

    添加文章路由处理函数

--- router/artcate.js
+++ router/artcate.js
@@ -2,9 +2,14 @@
 const express = require('express')
 const router = express.Router()
 
+// 引入文章列表处理函数
+const artcate_handler = require('../router_handler/artcate')
+
 // 获取文章分类列表数据和路由
-router.get('/cates', (req, res) => {
+/* router.get('/cates', (req, res) => {
 	res.send('ok')
-})
+}) */
+
+router.get('/cates', artcate_handler.getArtCate)
 
 module.exports = router
---
+++ router_handler/artcate.js
@@ -0,0 +1,6 @@
+// 路由处理函数模块
+
+// 获取文章分类列表的处理函数
+exports.getArtCate = (req, res) => {
+	res.send('ok')
+}
