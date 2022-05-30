0c524bbc1eaf1d281d1180a93c05f43ec5121e16
chenlim <chenlim233@163.com>
2022-05-30 14:31:32 +0800

    添加新增文章分类处理函数

--- router_handler/artcate.js
+++ router_handler/artcate.js
@@ -21,5 +21,32 @@ exports.getArtCate = (req, res) => {
 
 // 新增文章分类的处理函数
 exports.addArticleCates = (req, res) => {
-	res.send('新增ok')
+	// 定义查重的sql语句
+	const sql = 'select * from ev_article_cate where name=? or alias=?'
+
+	// 执行sql语句
+	db.query(sql, [req.body.name, req.body.alias], (err, result) => {
+		if (err) return res.cc(err)
+
+		// 执行成功，判断数据的length(4种情况)
+		if (result.length === 2) return res.cc('分类名称与分类别名被占用，请更换后重试')
+
+		if (result.length === 1 && result[0].name === req.body.name && result[0].alias === req.body.alias)
+			return res.cc('分类名称与分类别名被占用，请更换后重试')
+
+		if (result.length === 1 && result[0].name === req.body.name) return res.cc('分类名称被占用，请更换后重试')
+
+		if (result.length === 1 && result[0].alias === req.body.alias) return res.cc('分类别名被占用，请更换后重试')
+
+		// TODO：分类名称和分类别名都可用
+		// 定义插入文章分类的sql
+		const sql = 'insert into ev_article_cate set ?'
+		db.query(sql, req.body, (err, result) => {
+			if (err) return res.cc(err)
+
+			if (result.affectedRows !== 1) return res.cc('新增文章分类失败')
+
+			res.cc('新增文章分类成功！', 0)
+		})
+	})
 }
