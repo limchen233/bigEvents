ad63fb17c6b264697e905be9fccd641d1587e690
chenlim <chenlim233@163.com>
2022-05-31 11:27:37 +0800

    添加验证依赖,验证规则

--- router/article.js
+++ router/article.js
@@ -2,6 +2,19 @@
 const express = require('express')
 const router = express.Router()
 
+// 导入验证数据的中间件
+const expressJoi = require('@escook/express-joi')
+// 导入文章的验证模块
+const { add_article_schema } = require('../schema/article')
+
+// 导入解析 formdata 格式表单数据的包
+const multer = require('multer')
+// 导入处路径的核心模块
+const path = require('path')
+
+// 创建multer实例对象,通过dest属性指定文件的存放路径
+const upload = multer({ dest: path.join(__dirname, '../uploads') })
+
 // 发布文章的路由
 /* router.post('/add', (req, res) => {
 	res.send('ok')
@@ -10,6 +23,13 @@ const router = express.Router()
 // 导入路由处理模块
 const article_handler = require('../router_handler/article')
 
-router.post('/add', article_handler.addArticle)
+// router.post('/add', article_handler.addArticle)
+
+/* 发布新文章的路由
+  upload.single() 是一个局部生效的中间件，用来解析 FormData 格式的表单数据
+  将文件类型的数据，解析并挂载到 req.file 属性中
+  将文本类型的数据，解析并挂载到 req.body 属性中 
+*/
+router.post('/add', upload.single('cover_img'), expressJoi(add_article_schema), article_handler.addArticle)
 
 module.exports = router
--- router_handler/article.js
+++ router_handler/article.js
@@ -1,4 +1,5 @@
 // 发布文章处理模块
 exports.addArticle = (req, res) => {
-	res.send('ok')
+	// 手动判断是否上传了文章封面
+	if (!req.file || req.file.fieldname !== 'cover_img') return res.cc('文章封面是必选参数！')
 }
